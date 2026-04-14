d485d90ef70d2cd63bd5a31bede9b405d1a01490
leonisel <leonisel@outlook.com>
2026-04-14 19:09:20 +0500

    feat: Add sidebar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import './App.css'
+import './App.css';
+import { Button, Layout, Space } from 'antd';
+const { Sider, Content } = Layout;
 
 function App() {
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Sider width={200} style={{ padding: 16 }}>
+        <Space direction="vertical" style={{ width: '100%' }}>
+          <Button type="primary" block>
+            Add Input
+          </Button>
+          <Button block>Add Select</Button>
+          <Button block>Add Checkbox</Button>
+        </Space>
+      </Sider>
 
-  return <h1>Form Builder</h1>
+      <Content style={{ padding: 20 }}>
+        <h2>Form Preview</h2>
+      </Content>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
